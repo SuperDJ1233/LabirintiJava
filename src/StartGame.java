63143e7a8914de401b0e880e3984470db2d4e0e5
SuperDJ1233 <119948420+SuperDJ1233@users.noreply.github.com>
2024-01-09 22:56:56 +0100

    Shtim i hapjes se dritares

--- src/StartGame.java
+++ src/StartGame.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+import java.util.Date;
+
 public class StartGame {
     public static void main(String[] args){
-
+        String versioniLojes = "v.0.0.1";
+        JFrame frameKryesor = new JFrame(); //krijon objekt te dritares
+        frameKryesor.setTitle("Labirinti "+ versioniLojes + (new Date()).toString());
+        frameKryesor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //hide, exit, do_nothing
+        frameKryesor.setResizable(false);
+        frameKryesor.setSize(500, 500);
+        frameKryesor.setVisible(true); //e ben te dukshme dritren
     }
 }
