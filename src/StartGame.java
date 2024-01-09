56892c5a4d299a0e176fe9308d3512243c254394
SuperDJ1233 <119948420+SuperDJ1233@users.noreply.github.com>
2024-01-09 22:57:51 +0100

    Shtim i hapjes se dritares heqja e unessecery to string per daten2

--- src/StartGame.java
+++ src/StartGame.java
@@ -5,7 +5,7 @@ public class StartGame {
     public static void main(String[] args){
         String versioniLojes = "v.0.0.1";
         JFrame frameKryesor = new JFrame(); //krijon objekt te dritares
-        frameKryesor.setTitle("Labirinti "+ versioniLojes + new Date()).);
+        frameKryesor.setTitle("Labirinti "+ versioniLojes + new Date());
         frameKryesor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //hide, exit, do_nothing
         frameKryesor.setResizable(false);
         frameKryesor.setSize(500, 500);
